360063482230d2461344d9b57027f1e29f72ecc4
dparsley1203 <dparsley1203@gmail.com>
2021-08-23 16:17:58 -0500

    numbers show and add but not by name

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,29 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+
+const roll = () => {
+    const dieValue = Math.floor(Math.random() * 6) + 1
+
+    return dieValue
+
+}
+
+
+
+
+for (let i = 0; i < 10; i++) {
+    let die1 = roll()
+    let die2 = roll()
+
+    let message = `${die1} + ${die2}`
+    if (die1 === die2) {
+        message += " DOUBLES!"
+    }
+    console.log(message)
+
+
+}
+
