9a21cfc7ad8b748b1bbc926dc49d9e6d003b4e1e
dparsley1203 <dparsley1203@gmail.com>
2021-08-23 14:09:15 -0500

    transferred to java and tested

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,64 @@
 // Put your code here
+
+console.log("My Enemis List!")
+console.log("----------------")
+
+
+
+
+
+
+const getEnemies = () => {
+
+    const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: [
+            "Being a jerk to me in elementary school", 
+            "Not being nice to me in elementary school"
+        ],
+            IsReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses:  [
+                "Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"
+        ],
+            IsReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses:  [
+            "Phone calls in the theater",
+            "Phone calls on the bus",
+            "Phone calls in line at the grocery store",
+            "Poor conversationalist"
+        ],
+            IsReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses:  [
+            "Keeps giving me a hotplate"
+        ],
+            IsReallyHated: false
+    }
+    ]
+    return enemies
+}  
+
+console.log(getEnemies())
+
+for (const e of getEnemies()) {
+    if (e.IsReallyHated) {
+        console.log(`${e.firstName} ${e.lastName} (Really, really dislike!)`)
+    } else {
+        console.log(`${e.firstName} ${e.lastName}`)
+    }
+}
---
+++ place-names/csharp/CsharpToJava/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpToJava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
