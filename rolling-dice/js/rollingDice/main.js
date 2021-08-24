2c131c6a06762881d07b19932ae41bd1ba365029
dparsley1203 <dparsley1203@gmail.com>
2021-08-24 13:32:06 -0500

    added if else to translate numbers into words

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -18,12 +18,43 @@ for (let i = 0; i < 10; i++) {
     let die1 = roll()
     let die2 = roll()
 
+
+    //Long way to replace #s with words
+    // if (die1 === 1) {
+    //     die1 = "one"
+    // } else if (die1 === 2 ) {
+    //     die1 = "two"
+    // } else if (die1 === 3) {
+    //     die1 = "three"
+    // } else if (die1 === 4) {
+    //     die1 = "four"
+    // } else if (die1 === 5) {
+    //     die1 = "five"
+    // } else {
+    //     die1 = "six"
+    // }
+    
+    // if (die2 === 1) {
+    //     die2 = "one"
+    // } else if (die2 === 2) {
+    //     die2 = "two"
+    // } else if (die2 === 2) {
+    //     die2 = "two"
+    // } else if (die2 === 4) {
+    //     die2 = "four"
+    // } else if (die2 === 5) {
+    //     die2 = "five"
+    // } else {
+    //     die2 = "six"
+    // }
+
     let message = `${die1} + ${die2}`
     if (die1 === die2) {
         message += " DOUBLES!"
     }
     console.log(message)
-
-
+    
+    
+    
 }
 
